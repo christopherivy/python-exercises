3cfc39b0b6e9143f3c88817e5a32ef000fa76436
christopherivy <cdi.747@gmail.com>
2021-06-28 07:49:11 -0700

    python exercises

---
+++ count_up.py
@@ -0,0 +1,26 @@
+def count_up(start, stop):
+
+    # start count at first number until second number
+    while start <= stop:
+        # print the current number
+        print(start)
+        # increment start number
+        start += 1
+
+
+count_up(5, 17)
+
+"""Print all numbers from start up to and including stop.
+
+    For example:
+
+        count_up(5, 7)
+
+   should print:
+
+        5
+        6
+        7
+    """
+
+# YOUR CODE HERE
---
+++ words.py
@@ -0,0 +1,7 @@
+words = ["hello", "hey", "goodbye", "yo", "yes"]
+
+
+def print_upper_words():
+    for word in words:
+        if word[0] == 'h' or word[0] == 'y':
+            print(word)
